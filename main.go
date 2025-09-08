072a587790ab92bc998bc5eddcf77b53eb36e60d
GintoBos <114522869+GintoBos@users.noreply.github.com>
2025-09-08 20:35:11 +0200

    file server

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux() // Create a new ServeMux
+	const filepathRoot = "."
+	const port = 8080
+	mux := http.NewServeMux()
 
-	server := &http.Server{ // Create the server
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot))) // Serve static files from the root directory
+	server := &http.Server{                                  // Create the server
 		Addr:    ":8080", // Bind to localhost:8080
 		Handler: mux,     // Use our ServeMux
 	}
 
-	log.Println("Server starting on http://localhost:8080")
+	log.Printf("Server files from %s on %d", filepathRoot, port)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
