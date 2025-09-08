bd37bf7a32013a1d232c162724ab2afce43de43c
GintoBos <114522869+GintoBos@users.noreply.github.com>
2025-09-08 18:58:54 +0200

    added server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Learning HTTP Server project running!")
+	mux := http.NewServeMux() // Create a new ServeMux
+
+	server := &http.Server{ // Create the server
+		Addr:    ":8080", // Bind to localhost:8080
+		Handler: mux,     // Use our ServeMux
+	}
+
+	log.Println("Server starting on http://localhost:8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
