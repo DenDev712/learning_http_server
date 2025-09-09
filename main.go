0d349b79b0df6d79022ab8ce043c5ae0dab28038
GintoBos <114522869+GintoBos@users.noreply.github.com>
2025-09-09 19:46:16 +0200

    handlers and readiness endpoint

--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain ; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Ok"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	//for index.html
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	//for assets/logo.png
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+
+	//for readiness endpoint
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server{ // Create the server
 		Addr:    ":8080",
@@ -23,4 +32,5 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+
 }
