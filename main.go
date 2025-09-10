816f18f84b4158b2c9136f0f7de4273419ac5d1e
GintoBos <114522869+GintoBos@users.noreply.github.com>
2025-09-10 21:05:00 +0200

    removing unnecessary shit

--- main.go
+++ main.go
@@ -39,7 +39,6 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain ; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Ok"))
 }
 func main() {
 	cfg := &apiConfig{} //holds the counter
